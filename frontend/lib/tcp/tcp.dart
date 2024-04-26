aa0a172b88c011efbed32973c2b6947509947dd6
ShubhamVG <vgshubham49@gmail.com>
2024-04-26 22:19:48 +0530

    yes

--- frontend/lib/tcp/tcp.dart
+++ frontend/lib/tcp/tcp.dart
@@ -1,16 +1,26 @@
 import 'dart:io';
-// import 'dart:typed_data';
+import 'dart:typed_data';
 
 import '../bloc/connections_bloc/connections_bloc.dart';
+
+late final Socket socket;
 
 Future<void> tcpStuff({required ConnectionsBloc connectionsBloc}) async {
   const String serverIp = '127.0.0.1';
   const int serverPort = 8080;
 
-  Socket.connect(serverIp, serverPort).then((socket) {
-    socket.listen((data) {
-      final String str = String.fromCharCodes(data);
-      connectionsBloc.add(ConnectionAdded(str));
-    });
+  socket = await Socket.connect(serverIp, serverPort);
+
+  socket.listen((data) {
+    final String str = String.fromCharCodes(data);
+    connectionsBloc.add(ConnectionAdded(str));
   });
+}
+
+Future<void> sendData({
+  required int taskCode,
+  required Uint8List payload,
+}) async {
+  payload.insert(0, taskCode);
+  socket.write(payload);
 }
